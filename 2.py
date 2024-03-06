9de25c4c0381e5bfeb633827d16ef68287d51b26
glebik <152169562+Ebobolo@users.noreply.github.com>
2024-03-06 18:24:44 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,17 +1,13 @@
 import random
 
-# Create a sorted list of 10 random numbers
 numbers = sorted([random.randint(0, 100) for _ in range(10)])
+print(numbers)
+number_to_find = int(input("Введите число для поиска: "))
 
-# Get the number to search for from the user
-number_to_find = int(input("Enter a number to search for: "))
-
-# Implement the binary search algorithm
 def binary_search(numbers, target):
     left = 0
     right = len(numbers) - 1
 
-    # Continue searching until the left pointer is greater than the right pointer
     while left <= right:
         mid = (left + right) // 2
 
@@ -24,11 +20,10 @@ def binary_search(numbers, target):
 
     return -1
 
-# Call the binary search function with the sorted list and the number to find
 index = binary_search(numbers, number_to_find)
 
 # Print the result
 if index == -1:
-    print("That number is not in the list.")
+    print("Этого числа нет в списке")
 else:
-    print(f"The number {number_to_find} is at index {index} in the list.")
+    print(f"число {number_to_find} находится на индексе {index} в списке.")
